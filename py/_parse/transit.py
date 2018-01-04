aeee870cbd14408a94c46cb5b4691dae99eab280
tdeng075 <tdeng075@uottawa.ca>
2018-01-04 08:14:15 -0500

    useless one-liner

--- py/_parse/transit.py
+++ py/_parse/transit.py
@@ -1,8 +1,12 @@
-data = open("../../data/original/transit.txt").read().strip().splitlines()
-data = [i[:-i[::-1].find(",")-1] for i in data]
-data = [i[:i.find("(")]+i[i.find(")")+1:] if i.find("(")>0 else i for i in data]
+open("../../data/processed/transit_data.txt",'w').writelines((j+"\n" for j in [i[:i.find("(")]+i[i.find(")")+1:] if i.find("(")>0 else i for i in [i[:-i[::-1].find(",")-1] for i in open("../../data/original/transit.txt").read().strip().splitlines()]]))
 
-file = open("../../data/processed/transit_data.txt",'w')
-for i in data:
-    file.write(i + "\n")
-file.close()
+# derp :p
+
+# data = open("../../data/original/transit.txt").read().strip().splitlines()
+# data = [i[:-i[::-1].find(",")-1] for i in data]
+# data = [i[:i.find("(")]+i[i.find(")")+1:] if i.find("(")>0 else i for i in data]
+
+# file = open("../../data/processed/transit_data.txt",'w')
+# for i in data:
+#     file.write(i + "\n")
+# file.close()
